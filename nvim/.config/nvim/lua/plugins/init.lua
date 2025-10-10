ec8e99be0e435439c7ff15e40d1cb373c5774238
Vladimir Adamic <vladimir.adamic@gmail.com>
2025-10-10 22:43:01 +0200

    feat: convert plugins to lazy.nvim format
    - Convert all plugin specs from Packer to Lazy syntax
    - Mark startup-critical plugins with lazy = false
    - Add priority to colorschemes and CoC for load order
    - Packer still active, no functional changes yet
    Please enter the commit message for your changes. Lines starting

--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -1,5 +1,138 @@
 -- Lazy.nvim plugin specifications
--- This file will contain all plugin definitions once we migrate from Packer
+-- Converted from Packer format
 
--- For now, return empty table (Packer is still active)
-return {}
+return {
+  -- Core utilities
+  'tpope/vim-fugitive',
+  'tpope/vim-commentary',
+  'tpope/vim-surround',
+  'tpope/vim-repeat',
+  'tpope/vim-rhubarb',
+  'shumphrey/fugitive-gitlab.vim',
+  'tpope/vim-unimpaired',
+  'tpope/vim-sleuth',
+
+  -- Copilot (commented out, can enable if needed)
+  -- 'github/copilot.vim',
+
+  -- Telescope - fuzzy finder
+  {
+    'nvim-telescope/telescope.nvim',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+  },
+
+  -- Ripgrep integration
+  'jremmen/vim-ripgrep',
+
+  -- Icons (needed by many plugins, load at startup)
+  {
+    'kyazdani42/nvim-web-devicons',
+    lazy = false,
+  },
+
+  -- Buffer bar
+  {
+    'romgrk/barbar.nvim',
+    dependencies = { 'kyazdani42/nvim-web-devicons' },
+    lazy = false,
+  },
+
+  -- Status line
+  {
+    'nvim-lualine/lualine.nvim',
+    dependencies = { 'kyazdani42/nvim-web-devicons' },
+    lazy = false,
+  },
+
+  -- Git signs
+  {
+    'lewis6991/gitsigns.nvim',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    lazy = false,
+  },
+
+  -- Treesitter - syntax highlighting
+  {
+    'nvim-treesitter/nvim-treesitter',
+    lazy = false,
+    build = ':TSUpdate',
+  },
+
+  -- Treesitter textobjects
+  'nvim-treesitter/nvim-treesitter-textobjects',
+
+  -- LSP config (available but not actively used - using CoC instead)
+  'neovim/nvim-lspconfig',
+  'williamboman/nvim-lsp-installer',
+
+  -- Completion plugins (commented out - using CoC instead)
+  -- 'hrsh7th/nvim-cmp',
+  -- 'hrsh7th/cmp-nvim-lsp',
+  -- 'jose-elias-alvarez/null-ls.nvim',
+  -- 'saadparwaiz1/cmp_luasnip',
+  -- 'L3MON4D3/LuaSnip',
+
+  -- Which-key
+  'folke/which-key.nvim',
+
+  -- Formatter
+  'mhartington/formatter.nvim',
+
+  -- React extract
+  'napmn/react-extract.nvim',
+
+  -- Git linker
+  {
+    'ruifm/gitlinker.nvim',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+  },
+
+  -- Colorschemes (load at startup for theme)
+  {
+    'catppuccin/nvim',
+    name = 'catppuccin',
+    lazy = false,
+    priority = 1000, -- Load before other plugins
+  },
+
+  {
+    'Tsuzat/NeoSolarized.nvim',
+    lazy = false,
+  },
+
+  {
+    'folke/tokyonight.nvim',
+    lazy = false,
+  },
+
+  {
+    'sainnhe/everforest',
+    lazy = false,
+  },
+
+  -- Terminal
+  {
+    'akinsho/toggleterm.nvim',
+    version = '*',
+  },
+
+  -- Rainbow delimiters
+  'HiPhish/rainbow-delimiters.nvim',
+
+  -- TypeScript tools (commented out - using CoC instead)
+  -- {
+  --   "pmizio/typescript-tools.nvim",
+  --   dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
+  --   config = function()
+  --     require("typescript-tools").setup {}
+  --   end,
+  -- },
+
+  -- CoC.nvim - LSP and completion (must load at startup)
+  {
+    'neoclide/coc.nvim',
+    branch = 'release',
+    lazy = false,
+    priority = 900, -- Load early but after colorscheme
+  },
+}
