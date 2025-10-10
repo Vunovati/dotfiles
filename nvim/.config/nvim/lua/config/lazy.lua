5dddd96491e2368b5a69e5982c18ea096491fa0e
Vladimir Adamic <vladimir.adamic@gmail.com>
2025-10-10 22:40:37 +0200

    feat: add lazy.nvim bootstrap (not active yet)
    - Create lua/config/lazy.lua with bootstrap code
    - Create lua/plugins/ directory structure
    - Packer remains active, no functional changes
    Please enter the commit message for your changes. Lines starting

---
+++ nvim/.config/nvim/lua/config/lazy.lua
@@ -0,0 +1,38 @@
+-- Bootstrap lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+      { out, "WarningMsg" },
+      { "\nPress any key to exit..." },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- NOTE: Leader key should be set before loading lazy.nvim
+-- Currently using default leader key '\' (backslash)
+-- If you want to change it, uncomment and modify:
+-- vim.g.mapleader = " "
+-- vim.g.maplocalleader = "\\"
+
+-- Setup lazy.nvim
+require("lazy").setup({
+  spec = {
+    -- Import plugins from lua/plugins/
+    { import = "plugins" },
+  },
+  -- Install colorscheme used during installation
+  install = { colorscheme = { "catppuccin" } },
+  -- Automatically check for plugin updates
+  checker = { enabled = false }, -- Can enable after migration is stable
+  -- UI settings
+  ui = {
+    border = "rounded",
+  },
+})
---
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -0,0 +1,5 @@
+-- Lazy.nvim plugin specifications
+-- This file will contain all plugin definitions once we migrate from Packer
+
+-- For now, return empty table (Packer is still active)
+return {}
